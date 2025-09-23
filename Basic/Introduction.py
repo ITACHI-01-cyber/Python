37cbd48c2e1c1de4a2c577206df5dbb7ca421d85
VIVEK BHARDWAJ <bvivek514@gmail.com>
2025-09-23 12:20:48 +0000

    day01

---
+++ Basic/Introduction.py
@@ -0,0 +1,15 @@
+# hello world in Python
+
+print("hello World!!")
+
+# modules
+import math
+print(math.sqrt(16))
+
+# comments in Python
+class Solution:
+ def printValues(self, a, b, c):
+        print(a)
+       # print(b)
+        print(c)
+    # it not print the value of b.
