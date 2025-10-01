7e525a9be6c6b1ff7b36af85c7333acca9c8f626
VIVEK BHARDWAJ <bvivek514@gmail.com>
2025-10-01 13:01:35 +0000

    day 02

---
+++ Basic/datatypes.py
@@ -0,0 +1,46 @@
+# using variable in a type programm
+
+age = 18
+name = "vivek bhardwaj"
+wegiht = 70.6
+# print("my name is", name, "my age is", age, "my weight is", wegiht)
+
+# Using Variable in Computation
+
+price = 100
+tax = 12
+# print("the total pay is:", price + tax)
+
+# Using Pynamically Nature of python
+
+x = 20
+# print(x)
+x = "vivek bhardwaj"
+# print(x)
+
+# Swap of two numbers (basic)
+
+a = 10
+b = 20
+# print("before Swappingvalue of a is:", a)
+# print("before Swappingvalue of b is:", b)
+temp = a
+a = b
+b = temp
+# print("the value of a is:", a)
+# print("the value of b is:", b)
+
+# Swap of two numbers (advanced)
+
+a = 10
+b = 20
+# print("before Swappingvalue of a is:", a)   
+# print("before Swappingvalue of b is:", b)
+  
+# PYHONIC LOGIC 
+a,b = b,a
+# print("the value of a is:", a)
+# print("the value of b is:", b)
+
+
+
