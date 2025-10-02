d40bcae1e0ea56b3d2a0e82932f790dfbf4688f7
VIVEK BHARDWAJ <bvivek514@gmail.com>
2025-10-02 12:31:31 +0000

    day 03

---
+++ Basic/problem.py
@@ -0,0 +1,31 @@
+# Complex Problem
+
+list1 = [10,20,30]
+list2 = [30,10,20]
+print("list1 + list 2 = : ", list1 + list2)   #statement 1
+print( "list1 * 2 = : ", list1 * 2 ) #statement 2
+##############################
+tuple1 = (1, 2, 4, 3) 
+tuple2 = (1, 2, 3, 4) 
+print(tuple1 < tuple2) 
+###########################
+a = 11
+print(str(a))
+##########################
+a = 10
+b = 20
+c = 30
+print(a, b, c, sep="[.]")
+###############################
+print("Python", end="!")
+print("Rocks")
+############################
+age = input("Enter your age: ")
+print(type(age))
+######################`########
+x = 10
+y = 20
+print("Before swapping: ", x, y)
+x,y = y,x
+print("After swapping: ", x, y)
+###############################
